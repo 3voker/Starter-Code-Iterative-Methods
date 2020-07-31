df58007575c51486de4f190c6fa92ee5d71ed107
3voker <Drechristophe@gmail.com>
2020-07-31 09:52:38 -0500

    Added printKatas to DOM.

--- main.js
+++ main.js
@@ -29,35 +29,49 @@ function printKata (kataNumber, object) {
 const isActiveFunction = users.filter(user => user.isActive === true)
 isActiveFunction.kataNumber = 1;
  //|| users.filter(function (user) {return user.isActive === true;})
-
+printKata(1, users[0])
 //Kata 2a (Arrow Function)                         //Kata 2b  Functional Expression
 const showEmail = users.map(user => user.email) || users.map(function (user) {return user.email;})
 showEmail.kataNumber = 2;
+printKata(2, users[1])
+
 //Kata 3a (Arrow Function)                                            //Kata 3b  Functional Expression
 const showCompany = users.some(user => user.company === "OVATION") || users.some(function (user) {return user.company;})
  showCompany.kataNumber = 3;
+ printKata(3, users[2])
+
 //Using || for functions for find can create buggy issues. 
-//Kata 4a (Arrow Function).find                          //Kata 4b  Functional Expression
+//Kata 4a (Arrow Function).find  
+printKata(4, users[3]) 
+                       //Kata 4b  Functional Expression
 const showAge = users.find(user => user.age === 38) //|| users.find(function (user) {return user.age;})
 showAge.kataNumber = 4;
+printKata(5, users[4])
+
 //Kata 5a (Arrow Function) .filter .find                                                     //Kata 5b  Functional Expression
 const toUsersOver38 = user => user.age >= 38
 toUsersOver38.kataNumber = 5;
+printKata(6, users[5])
+
 const active = user => user.isActive === true
 active.kataNumber = 6;
+printKata(7, users[6])
+
 const showGenXisActive = users
     .filter(toUsersOver38)
     .find(active)
 showGenXisActive.kataNumber = 7;
+printKata(8, users[7])
 
 bob = users.push(user => {name = 'Bobby', email ='bobbykins@hotmail.com'})
 
 
 //Kata 6a (Arrow Function) .filter .map
-const showBalance = users.filter(user => users.company === "ZENCO").map(user => users.balance) 
-//Kata 6b
+const showBalance = users.filter(user => users.company === "ZENCO").map(user => users.balance)
 showBalance.kataNumber = 8;
+printKata(8, users[7])
+
 //Kata 7a (Arrow Function) .filler .includes .map
 const fugginAge = users.filter(user => users.age).includes("fugiat")
-fugginAge.kataNumber = 9;  
-//Kata 7b
+fugginAge.kataNumber = 9;
+printKata(9, users[8])
